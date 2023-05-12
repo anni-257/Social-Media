d9f32535719a52e0386d29638ccc676fc09f214d
anni-257 <taraleaniket257@gmail.com>
2023-05-12 11:26:01 +0530

    this is main entry point of react-app

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const dotenv=require("dotenv");
+dotenv.config("./.env");
+const mongoConnect=require("./dbConnect")
+const authRouter=require("./routers/authRouter");
+const postsRouter=require("./routers/postsRouter");
+const morgan = require("morgan");
+
+const app=express();
+
+// Middlewares
+app.use(morgan("common"))
+app.use(express.json());
+app.use('/auth',authRouter);
+app.use('/posts',postsRouter);
+
+app.use('/',(req,res)=>{
+    return res.send("Okay From Server side");
+})
+
+const PORT= process.env.PORT || 3000;
+
+mongoConnect();
+
+app.listen(PORT,()=>{
+    console.log("listening on port number: "+PORT);
+})
