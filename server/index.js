47384944e6ba2f60f5c75f5f6209526afb7e90ea
anni-257 <taraleaniket257@gmail.com>
2023-05-13 13:00:51 +0530

    cookie-parser package added

--- server/index.js
+++ server/index.js
@@ -5,15 +5,17 @@ const mongoConnect=require("./dbConnect")
 const authRouter=require("./routers/authRouter");
 const postsRouter=require("./routers/postsRouter");
 const morgan = require("morgan");
+const cookieParser=require("cookie-parser");
 
 const app=express();
 
 // Middlewares
 app.use(morgan("common"))
 app.use(express.json());
+app.use(cookieParser());
 app.use('/auth',authRouter);
 app.use('/posts',postsRouter);
-
+ 
 app.use('/',(req,res)=>{
     return res.send("Okay From Server side");
 })
